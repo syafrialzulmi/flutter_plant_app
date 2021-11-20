cda0127804f05693cd6c8ae8112af08f70f5bc22
Syafrial Zulmi <syafrialzulmi@gmail.com>
2021-11-20 11:20:05 +0700

    footer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +24,7 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff6D8A51),
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -45,16 +47,184 @@ class MyHome extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Flexible(
-            flex: 2,
-            child: Container(
-              color: Colors.amber,
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.6,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(174),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[Text('data'), Text('data')],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('data'),
+                    Column(
+                      children: <Widget>[
+                        Text('data'),
+                        Row(
+                          children: <Widget>[Text('data'), Text('data')],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
-          Flexible(
-            flex: 1,
-            child: Container(
-              color: Colors.pink,
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.2,
+            color: Color(0xff6D8A51),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: SvgPicture.asset(
+                          "assets/icons/Height.svg",
+                          height: 27,
+                          width: 27,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Height',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 14,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 9),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 2,
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              6,
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          '45 cm - 55 cm',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 10,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: SvgPicture.asset(
+                          "assets/icons/Temperature.svg",
+                          height: 27,
+                          width: 27,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Temperature',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 14,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 9),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 2,
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              6,
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          '20 C to 26 C',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 10,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: SvgPicture.asset(
+                          "assets/icons/Pot.svg",
+                          height: 27,
+                          width: 27,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Pot',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 14,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 9),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 2,
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              6,
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          'Self Watering Pot',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 10,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
